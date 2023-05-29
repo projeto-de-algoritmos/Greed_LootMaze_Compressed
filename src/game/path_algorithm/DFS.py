9c8a592d1d0c6ff09be746302c5e2ee1ea2afb39
Cainã Valença de Freitas <caina2000@gmail.com>
2023-05-29 20:28:56 -0300

    fixes once

--- src/game/path_algorithm/DFS.py
+++ src/game/path_algorithm/DFS.py
@@ -2,21 +2,22 @@ class DFS:
     def __init__(self, grid):
         self.name = "DFS"
         self.grid = grid
-        self.start = self.grid.spawn
+        self.spawn = self.grid.spawn
         self.goal = self.grid.goal
 
-        self.stack = [self.start]  # Use a stack instead of a set for DFS
-        self.came_from = {self.start: None}
+        self.open_set = {grid.spawn}
+        self.came_from = {grid.spawn: None}
         self.explored = set()
 
     def reset(self):
-        self.stack = [self.start]
-        self.came_from = {self.start: None}
+        self.open_set = {self.spawn}
+        self.came_from = {self.spawn: None}
         self.explored = set()
 
     def get_neighbors(self, cell):
         # Returns walkable neighbors
         neighbors = []
+        print(cell)
         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             nx, ny = cell[0] + dx, cell[1] + dy
             if (
@@ -28,23 +29,27 @@ class DFS:
         return neighbors
 
     def algorithm_tick(self):
-        if not self.stack:
+        if not self.open_set:
             return None, self.explored  # No path found
 
-        current = self.stack.pop()  # Take the last cell from the stack
+        # Take the last cell from open_set
+        current = self.open_set.pop()
         self.explored.add(current)  # Add current cell to explored set
 
         if current == self.goal:
             path = []
-            while current is not None and current != self.start:
+            while current is not None and current != self.spawn:
                 path.append(current)
                 current = self.came_from[current]
             path.reverse()
             return path, self.explored
 
         for neighbor in self.get_neighbors(current):
-            if neighbor not in self.explored and neighbor not in self.stack:
-                self.stack.append(neighbor)  # Add neighbors to the stack
+            if neighbor not in self.explored:
                 self.came_from[neighbor] = current
 
+                if neighbor not in self.open_set:
+                    self.open_set.add(neighbor)
+
         return None, self.explored  # Path not yet found
+
